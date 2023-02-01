cb4d046a768c3b4e1a3b27df362198933a33065c
Ashraf Abdelrahman <96283360+AshrafAKRahman@users.noreply.github.com>
2023-02-01 11:25:32 +0000

    added the greeting and the time components

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Bookmarks from "./Bookmarks/Bookmarks";
+import Greeting from "./Greeting/Greeting";
+import Time from "./Time/Time";
 
 function App() {
   return (
     <div className="app-wrapper">
       <div className="App">
         <Bookmarks />
+        <Greeting />
+        <Time />
       </div>
     </div>
   );
